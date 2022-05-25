8fb95e7d24fe735068479d93d5fb7a82892ce28c
ali <aliemamalinezhad@gmail.com>
2022-05-25 18:58:43 +0430

    changes in urls.py, create schema.py for graphql

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 # from graphene_django.views import GraphQLView
-# from post.schema import schema
+# from post.api.schema import schema
 
 schema_view = get_schema_view(
     openapi.Info(
